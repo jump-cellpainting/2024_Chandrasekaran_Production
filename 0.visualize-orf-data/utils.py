13c62557e483aff72c145de87416811ea89eb924
Niranj <srinivasniranj@gmail.com>
2022-11-18 20:09:28 -0500

    ORF UMAP

---
+++ 0.visualize-orf-data/utils.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+
+def load_data(batch, plate, filetype):
+    """load all data from a single experiment into a single dataframe"""
+    path = os.path.join('../jump-orf-data/profiles',
+                        f'*{batch}',
+                        f'{plate}',
+                        f'*_{filetype}')
+    files = glob.glob(path)
+    df = pd.concat(pd.read_csv(_, low_memory=False) for _ in files)
+    return df
+
+
+def get_metacols(df):
+    """return a list of metadata columns"""
+    return [c for c in df.columns if c.startswith("Metadata_")]
+
+
+def get_featurecols(df):
+    """returna  list of featuredata columns"""
+    return [c for c in df.columns if not c.startswith("Metadata")]
+
+
+def get_metadata(df):
+    """return dataframe of just metadata columns"""
+    return df[get_metacols(df)]
+
+
+def get_featuredata(df):
+    """return dataframe of just featuredata columns"""
+    return df[get_featurecols(df)]
+
+
+def remove_negcon_empty_wells(df):
+    """return dataframe of non-negative control wells"""
+    df = (
+        df.query('Metadata_control_type!="negcon"')
+        .dropna(subset=['Metadata_broad_sample'])
+        .reset_index(drop=True)
+    )
+    return df
+
+
+def remove_empty_wells(df):
+    """return dataframe of non-empty wells"""
+    df = (
+        df.dropna(subset=['Metadata_broad_sample'])
+        .reset_index(drop=True)
+    )
+    return df
+
+
+def concat_profiles(df1, df2):
+    """Concatenate dataframes"""
+    if df1.shape[0] == 0:
+        df1 = df2.copy()
+    else:
+        frames = [df1, df2]
+        df1 = pd.concat(frames, ignore_index=True, join="inner")
+
+    return df1
